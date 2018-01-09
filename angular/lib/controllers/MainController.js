bb6862f4e7dcfca881887a8760c5f51a9aeb7b79
Patryk <patrykbaklaj@gmail.com>
2018-01-09 22:43:36 +0100

    Add menu section and Google Map

--- angular/lib/controllers/MainController.js
+++ angular/lib/controllers/MainController.js
@@ -2,6 +2,35 @@
 
 RockPizza.controller('MainController', function($scope){
 
-  $scope.name = "Ala ma kota";
+  $scope.pizzas = [
+    {
+      name: "Margherita",
+      ingredients: [
+        "ser",
+        "sos pomidorowy"
+      ],
+      popularity: 0,
+    },
+    {
+      name: "Capriciosa",
+      ingredients: [
+        "ser",
+        "sos pomidorowy",
+        "szynka",
+        "pieczarki"
+      ],
+      popularity: 12,
+    },
+    {
+      name: "Salame",
+      ingredients: [
+        "ser",
+        "sos pomidorowy",
+        "salami",
+        "cebula"
+      ],
+      popularity: 3,
+    }
+  ];
 
 });
